f1068e7047547183b42ca1d852065617b9abbdc1
SimranGur9335 <simran.gur@example.com>
2026-06-05 19:16:22 +0530

    Added placement session files

---
+++ Questions performed/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int chinki=1; chinki<=20; chinki++){
+            if (chinki%2==0){
+                System.out.println(chinki);
+            }
+        }
+    }
+}
