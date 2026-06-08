9b667a7db3c7f0a0636da2ba4f510a5e8b058657
Janmesh Raut <janmeshraut0607@gmail.com>
2026-06-08 14:36:50 +0530

    Refactor Employee class methods for improved readability and structure

--- String/Employee.java
+++ String/Employee.java
@@ -1,53 +1,80 @@
-
-
 import java.util.Scanner;
 
 public class Employee {
+
+    static void displayEmployees(String[] employee) {
+        System.out.println("Employee Names:");
+        for (String name : employee) {
+            System.out.println(name);
+        }
+    }
+
+    static void searchByFirstName(String[] employee, String firstName) {
+        for (String name : employee) {
+            if (name.startsWith(firstName)) {
+                System.out.println("Found: " + name);
+            }
+        }
+    }
+
+    static void searchByLastName(String[] employee, String lastName) {
+        for (String name : employee) {
+            if (name.endsWith(lastName)) {
+                System.out.println("Found: " + name);
+            }
+        }
+    }
+
+    static void countEmployees(String[] employee) {
+        System.out.println("Total Employees: " + employee.length);
+    }
+
+    static void longestName(String[] employee) {
+        String longest = employee[0];
+
+        for (String name : employee) {
+            if (name.length() > longest.length()) {
+                longest = name;
+            }
+        }
+
+        System.out.println("Longest Name: " + longest);
+    }
+
+    static void convertToUpperCase(String[] employee) {
+        System.out.println("Employee Names in Upper Case:");
+        for (String name : employee) {
+            System.out.println(name.toUpperCase());
+        }
+    }
+
     public static void main(String[] args) {
-        String employee[]={
+
+        String[] employee = {
             "Shlok Oberoi",
             "Rohit Sharma",
             "Virat Kohli",
             "Aman Singh"
         };
 
-        System.out.println("Employee Name:");
-        for(String name:employee){
-            System.out.println(name);
-        }
+        Scanner sc = new Scanner(System.in);
 
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Employee First Name");
-        String firstName=sc.next();
+        displayEmployees(employee);
 
-        for(String name:employee){
-            if(name.startsWith(firstName)){
-                System.out.println("Found:"+name);
-            }
-            
-        }
-        System.out.println("Enter Employee Last Name");
-        String lastName=sc.next();
+        System.out.print("\nEnter Employee First Name: ");
+        String firstName = sc.next();
+        searchByFirstName(employee, firstName);
 
-        for(String name :employee){
-            if(name.endsWith(lastName)){
-                System.out.println("Found:"+name);
-            }
-        }
-        System.out.println("Total Employee: "+employee.length);
+        System.out.print("\nEnter Employee Last Name: ");
+        String lastName = sc.next();
+        searchByLastName(employee, lastName);
 
-        String longestName=employee[0];
+        countEmployees(employee);
 
-        for(String name:employee){
-            if(longestName.length()<name.length()){
-                longestName=name;
-            }
-        }
+        longestName(employee);
 
-        System.out.println("Longest Name:"+longestName);
-        System.out.println("Employee Name in Upper Case:");
-        for(String name:employee){
-            System.out.println(name.toUpperCase());
-        }
+        convertToUpperCase(employee);
+
+        sc.close();
     }
 }
